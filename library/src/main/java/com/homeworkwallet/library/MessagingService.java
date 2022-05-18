8926577600aeb2b25e7f52141fb8278b07bf2bb9
Lorens Vernickis <lorens.vernickis@norwegian.com>
2022-05-18 21:10:32 +0300

    Cleaning and refactoring

---
+++ library/src/main/java/com/homeworkwallet/library/MessagingService.java
@@ -0,0 +1,11 @@
+package com.homeworkwallet.library;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessagingService {
+
+    public String sendMessage() {
+        return "mockSendMessage";
+    }
+}
